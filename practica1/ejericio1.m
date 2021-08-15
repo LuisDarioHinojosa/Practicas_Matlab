8bdfd6bf1520be85240ebdd2b21a2cf90ae5f8f2
Luis Dario <dariophoskyhinojosa@gmail.com>
2021-08-14 23:06:05 -0500

    P1 | 1

---
+++ practica1/ejericio1.m
@@ -0,0 +1,48 @@
+% senoidan funciont taking ampplitude and frequency as user inputs
+
+in1 = 'Enter amplitude: \n';
+in2 = 'Enter frequency: \n';
+
+
+a = input(in1);
+w = input(in2);
+
+
+t2  = [-pi : 0.01 :pi];
+y = a*sin(t2 * w);
+
+figure()
+plot(t2,y,'r')
+format = [a,w];
+txt = sprintf(' %d * Sin( t * %d )',format);
+title('Customized Senoidal')
+xlabel('x')
+ylabel(txt)
+grid on
+
+
+% First part of the exercise
+
+
+% timesteps vector
+t = [-3 : 0.001 :3 ];
+
+% x(t)= 5t^2+2t
+
+x = 5*power(t,2) + 2 * t;
+
+figure()
+plot(t,x,'b')
+
+title('Function: x(t) =  5t^2+2t')
+xlabel('timesteps')
+ylabel('x(t)')
+grid on
+
+
+
+
+
+
+
+
