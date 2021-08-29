b315c04dc2f7ab12811d605a6f321d181a5effba
Luis Dario <dariophoskyhinojosa@gmail.com>
2021-08-28 19:54:03 -0500

    etapa 3 almost finished

--- Situacion Problema/Etapa3.m
+++ Situacion Problema/Etapa3.m
@@ -1,9 +1,9 @@
 clear
 clc
 
-q=5;
+q=3;
 f1=2500;
-%% Record your voice for q seconds.
+% Record your voice for q seconds.
 recObj = audiorecorder;
 disp('Start speaking.')
 recordblocking(recObj, q);
@@ -18,13 +18,14 @@ myRecording = getaudiodata(recObj);
 qa=recObj.TotalSamples;
 t=(0:q/qa:q-q/qa)';
 
-%% for the frrequency axis
+% for the frrequency axis
 Ts=q/qa; %sampling time
 fs=1/Ts; %sampling frequency
 [na,nb]=size(t(:)); % na=number of points of signal
 ff=fs*[0:na-1]/na-fs/2;
 
 % Plot the waveform.
+figure()
 plot(t,myRecording);
 xlabel('time (secs)')
 ylabel('amplitude (V)')
@@ -38,6 +39,64 @@ plot(ff,fftshift(abs(fft(myRecording))),'r')
 % hold off
 xlabel('frequency (Hz)')
 ylabel('Magnitude (V)')
+
+
+
+% statitical variables 
+
+skw = skewness(myRecording)
+kurt = kurtosis(myRecording)
+tMean = mean(myRecording)
+stDev = std(myRecording)
+v = var(myRecording)
+deviation = v/stDev
+
+
+% histogram
+figure()
+histogram(myRecording, 10)
+
+
+% autocorreltaion:
+
+[c_ww,lags] = xcorr(myRecording,10,'coeff');
+figure()
+stem(lags,c_ww);
+title("Audio Autocorrelation")
+
+
+% probability density function
+xDist = linspace(-0.1,0.1,length(t));
+pd = makedist("Normal",mean(myRecording),std(myRecording));
+pdfT = pdf(pd,xDist);
+% cumulative distribution Fucntion
+cdfT = cdf(pd,xDist);
+
+figure()
+subplot(1,2,1)
+plot(xDist,pdfT,'LineWidth',2)
+title("Probability Density Function");
+xlabel("audio values")
+ylabel("probability")
+subplot(1,2,2)
+plot(xDist,cdfT,'LineWidth',2)
+title("Cumulative Distribution Function");
+xlabel("audio values")
+ylabel("probability")
+
+
+
+% Power Spectral Density
+N = 20; % lenght of the overall fourier transform
+Ns = length(myRecording);
+psd = abs(fft(myRecording,N)).^2/Ns;
+figure();
+stem( (0:(N-1))/N,psd )
+
+
+
+
+
 
 
 
